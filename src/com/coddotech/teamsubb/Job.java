c50ad83041f48972bde9d91f9a741de3f29b3e35
Coddo <codobanclaudiu@gmail.com>
2013-12-05 16:39:55 +0200

    Created new classes for Job entities

---
+++ src/com/coddotech/teamsubb/Job.java
@@ -0,0 +1,17 @@
+package com.coddotech.teamsubb;
+
+/**
+ * Entity used by the JobManager class. This class stores information about a
+ * job and can passed thoughout the user network
+ * 
+ * @author Coddo
+ * 
+ */
+public final class Job {
+	/**
+	 * Class constructor
+	 */
+	public Job() {
+
+	}
+}
---
+++ src/com/coddotech/teamsubb/JobManager.java
@@ -0,0 +1,21 @@
+package com.coddotech.teamsubb;
+
+import java.util.ArrayList;
+
+/**
+ * Class used for managing jobs and their details
+ * 
+ * @author Coddo
+ * 
+ */
+public final class JobManager {
+	
+	private ArrayList<Job> jobs;
+	
+	/**
+	 * Class constructor
+	 */
+	public JobManager() {
+		jobs = new ArrayList<Job>();
+	}
+}
