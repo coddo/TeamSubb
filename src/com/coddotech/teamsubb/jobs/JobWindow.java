7cc2edd04800544a95fb9a33efd26943ef8c8144
Coddo <codobanclaudiu@gmail.com>
2013-12-28 23:32:39 +0200

    Backup

--- src/com/coddotech/teamsubb/jobs/JobWindow.java
+++ src/com/coddotech/teamsubb/jobs/JobWindow.java
@@ -38,12 +38,22 @@ public class JobWindow extends CustomWindow implements Observer {
 	private Group jobs;
 	private Group jobInfo;
 	private Group helpChart;
-	
-	//menu bar objects
 	private Menu menuBar;
+
+	// menu bar objects
+	private MenuItem applicationMenuItem;
+	private MenuItem aboutMenuItem;
+	private MenuItem jobsMenuItem;
+	private MenuItem actionsMenuItem;
 	
-	// menu objects
-	private Menu actions;
+	// application menu objects
+	private MenuItem openSettingsMenuItem;
+	
+	// jobs menu objects
+	
+	
+	// actions menu objects
+	private Menu actionsMenu;
 
 	public JobWindow(String[] userInfo, String[] userJobs) {
 		super();
@@ -57,7 +67,30 @@ public class JobWindow extends CustomWindow implements Observer {
 	}
 
 	public void dispose() {
+		//controller
 		controller.dispose();
+
+		//window objects
+		userInfo.dispose();
+		userJobs.dispose();
+		jobs.dispose();
+		jobInfo.dispose();
+		helpChart.dispose();
+		
+		//menu bar objects
+		menuBar.dispose();
+		applicationMenuItem.dispose();
+		jobsMenuItem.dispose();
+		actionsMenuItem.dispose();
+		aboutMenuItem.dispose();
+		
+		// application menu objects
+		openSettingsMenuItem.dispose();
+		
+		//actions menu objects
+		actionsMenu.dispose();
+		
+		// jobs menu objects
 	}
 
 	public JobController getController() {
@@ -65,7 +98,7 @@ public class JobWindow extends CustomWindow implements Observer {
 	}
 
 	@Override
-	public void update(Observable arg0, Object arg1) {
+	public void update(Observable obs, Object obj) {
 		// TODO Auto-generated method stub
 
 	}
@@ -74,43 +107,63 @@ public class JobWindow extends CustomWindow implements Observer {
 	protected void performInitializations() {
 		controller = new JobController(this);
 
+		// window objects
 		userInfo = new Group(this.getShell(), SWT.None);
 		userJobs = new Group(this.getShell(), SWT.None);
 		jobs = new Group(this.getShell(), SWT.None);
 		jobInfo = new Group(this.getShell(), SWT.None);
 		helpChart = new Group(this.getShell(), SWT.None);
-		
 		menuBar = new Menu(this.getShell(), SWT.BAR);
+		
+		//menu bar objects
+		applicationMenuItem = new MenuItem(menuBar, SWT.CASCADE);
+		jobsMenuItem = new MenuItem(menuBar, SWT.CASCADE);
+		actionsMenuItem = new MenuItem(menuBar, SWT.CASCADE);
+		aboutMenuItem  = new MenuItem(menuBar, SWT.CASCADE);
+		
+		//applications menu objects
+		openSettingsMenuItem = new MenuItem(parent, style)
 	}
 
 	@Override
 	protected void createObjectProperties() {
-		userInfo.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1));
+		// groups
+		userInfo.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1,
+				1));
 		userInfo.setText("User information");
 
-		userJobs.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1));
+		userJobs.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1,
+				1));
 		userJobs.setText("User abilities");
 		userJobs.setOrientation(SWT.RIGHT_TO_LEFT);
-		
+
 		jobs.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
 		jobs.setText("Job lists");
-		
+
 		jobInfo.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 		jobInfo.setText("Job information");
-		
-		helpChart.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+
+		helpChart.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1,
+				1));
 		helpChart.setText("Help chart");
+		helpChart.setOrientation(SWT.RIGHT_TO_LEFT);
+		
+		//menu bar items
+		applicationMenuItem.setText("Application");
+		jobsMenuItem.setText("Jobs");
+		actionsMenuItem.setText("Actions");
+		aboutMenuItem.setText("About");
 	}
 
 	@Override
 	public void createShellProperties() {
-		 GridLayout layout = new GridLayout();
-		 layout.numColumns = 2;
-		 
-		 this.getShell().setLayout(layout);
-		 this.getShell().setSize(800, 600);
-		 this.getShell().setMenuBar(menuBar);
-		 this.placeToCenter();
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 2;
+
+		this.getShell().setLayout(layout);
+		this.getShell().setSize(800, 600);
+		this.getShell().setMenuBar(menuBar);
+		this.placeToCenter();
 	}
 
 	@Override
