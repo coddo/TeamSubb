5edaee83dcaca1445bcefbce40f016a5094e5dbb
Coddo <codobanclaudiu@gmail.com>
2014-01-14 12:35:56 +0200

    Changed ActivityLoggger structure

--- src/com/coddotech/teamsubb/logging/ActivityLogger.java
+++ src/com/coddotech/teamsubb/logging/ActivityLogger.java
@@ -9,37 +9,58 @@ import java.io.File;
  * creating the dump file containing the error details.
  * 
  * This class is a singleton.
+ * 
  * @author Coddo
  * 
  */
 public class ActivityLogger {
-	
-	private static File LOG_FILE = new File("");
-	private static File DUMP_FILE = new File("");
+
+	private static String LOG_FOLDER_PATH = System.getProperty("user.dir")
+			+ File.separator + "DUMP" + File.separator;
+
+	private File logFile;
+	private File dumpFile;
 
 	private static ActivityLogger instance = null;
-	
+
 	private ActivityLogger() {
-		
+		File logFolder = new File(ActivityLogger.LOG_FOLDER_PATH);
+
+		try {
+			if (!logFolder.exists())
+				logFolder.mkdir();
+		} catch (Exception ex) {
+		}
+
+		initializeLogFiles();
 	}
-	
-	public ActivityLogger getInstance() {
+
+	public static ActivityLogger getInstance() {
 		if (instance == null)
 			instance = new ActivityLogger();
-		
+
 		return instance;
 	}
-	
-	public void log(String className, String activity, String result) {
-		
+
+	public void logActivity(String className, String activity) {
+
 	}
-	
+
+	public void logError(String className, String activity, String errorType,
+			String message) {
+
+	}
+
 	public void createDump(Exception ex) {
-		
+
 	}
-	
+
 	public void saveLogFile() {
-		
+
 	}
-	
+
+	private void initializeLogFiles() {
+
+	}
+
 }
