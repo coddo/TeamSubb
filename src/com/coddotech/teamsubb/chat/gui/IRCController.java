3c7a72eed8dc4bc3bcec62138dfc328a6227ab80
Coddo <codobanclaudiu@gmail.com>
2014-02-06 16:29:15 +0200

    Added components for creating the Chat feature

---
+++ src/com/coddotech/teamsubb/chat/gui/IRCController.java
@@ -0,0 +1,40 @@
+package com.coddotech.teamsubb.chat.gui;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+
+
+public class IRCController {
+
+	IRCWindow view;
+	
+	public IRCController(IRCWindow view) {
+		this.view = view;
+	}
+	
+	public void dispose() {
+		
+	}
+	
+	public Listener shellClosingListener = new Listener() {
+
+		@Override
+		public void handleEvent(Event arg0) {
+			view.dispose();
+
+		}
+	};
+
+	public Listener keyPressed = new Listener() {
+
+		@Override
+		public void handleEvent(Event e) {
+
+			if (e.detail == SWT.TRAVERSE_RETURN) {
+				
+			}
+
+		}
+	};
+}
