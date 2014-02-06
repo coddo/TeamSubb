78e7c11fdcee3f0b3e8567b243765031165aa519
Coddo <codobanclaudiu@gmail.com>
2014-02-06 16:49:04 +0200

    Small backup before working on other branches

--- src/com/coddotech/teamsubb/chat/model/StaffManager.java
+++ src/com/coddotech/teamsubb/chat/model/StaffManager.java
@@ -1,5 +1,7 @@
 package com.coddotech.teamsubb.chat.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 import com.coddotech.teamsubb.connection.model.ConnectionManager;
@@ -14,6 +16,51 @@ import com.coddotech.teamsubb.main.CustomWindow;
  */
 public class StaffManager {
 
+	@SuppressWarnings ("unused")
+	private class Staff {
+
+		int ID;
+		String name;
+		String jobs;
+
+	}
+
+	List<Staff> staff = new ArrayList<Staff>();
+
+	public void dispose() {
+		staff.clear();
+
+	}
+
+	public Staff[] getStaff() {
+		return this.staff.toArray(new Staff[staff.size()]);
+		
+	}
+
+	/**
+	 * Refresh the list containing the staff details (fethes the data from the server)
+	 */
+	public void refreshStaffList() {
+		String[] staffData = StaffManager.fetchStaffList(true);
+
+		if (staffData == null)
+			return;
+
+		staff.clear();
+
+		for (String data : staffData) {
+			String[] split = data.split(":");
+
+			Staff member = new Staff();
+
+			member.ID = Integer.parseInt(split[0]);
+			member.name = split[1];
+			member.jobs = split[2];
+
+			staff.add(member);
+		}
+	}
+
 	/**
 	 * Fetch the staff list from the server.<br>
 	 * The list contains both the name of the staff member, and his/her attributions. <br>
@@ -27,13 +74,13 @@ public class StaffManager {
 	 * 
 	 * @return A String collection
 	 */
-	public static String[] getStaffList() {
+	public static String[] fetchStaffList(boolean includeID) {
 		if (CustomWindow.isConnected(false)) {
 
 			String[] staffData = ConnectionManager.sendStaffRequest().split(JobManager.SEPARATOR_JOBS);
 
 			for (int i = 0; i < staffData.length; i++) {
-				staffData[i] = StaffManager.createStaffString(staffData[i]);
+				staffData[i] = StaffManager.createStaffString(staffData[i], includeID);
 
 			}
 
@@ -43,17 +90,31 @@ public class StaffManager {
 		return null;
 	}
 
-	public static String[] getStaffDetails() throws UnsupportedOperationException {
-		throw new UnsupportedOperationException();
-	}
-
-	public static String[] getStaffJobs() throws UnsupportedOperationException {
-		throw new UnsupportedOperationException();
-
-	}
-
-	private static String createStaffString(String staffData) {
+	/**
+	 * Create an easier to read String out of the raw message received from the server.<br>
+	 * <br>
+	 * 
+	 * The two possible structures are: <br>
+	 * -NAME: Job1 | Job2 | Job3....
+	 * -ID: NAME: Job1 | Job2 | Job3....
+	 * 
+	 * @param staffData
+	 *            The raw message from the server
+	 * @param includeID
+	 *            Indicates whether to include the ID of the user in the String
+	 * 
+	 * @return A String value
+	 */
+	private static String createStaffString(String staffData, boolean includeID) {
 		staffData = staffData.replaceAll(JobManager.SEPARATOR_FIELDS, " | ");
+
+		if (!includeID) {
+			staffData = staffData.replace(staffData.substring(0, staffData.indexOf(" | ") + 3), "");
+
+		}
+
+		else
+			staffData = staffData.replaceFirst(Pattern.quote(" |"), ":");
 
 		staffData = staffData.replaceFirst(Pattern.quote(" |"), ":");
 
