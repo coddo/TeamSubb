13066036abdf38b83586add1b0c4e60dbf8e2537
Coddo <codobanclaudiu@gmail.com>
2014-02-10 00:44:41 +0200

    Activated the message timer and made all the chat methods async

--- src/com/coddotech/teamsubb/chat/model/Messaging.java
+++ src/com/coddotech/teamsubb/chat/model/Messaging.java
@@ -4,6 +4,7 @@ import java.util.Observable;
 
 import com.coddotech.teamsubb.connection.model.ConnectionManager;
 import com.coddotech.teamsubb.notifications.model.NotificationEntity;
+import com.coddotech.teamsubb.timers.MessageTimer;
 
 public class Messaging extends Observable {
 
@@ -20,6 +21,11 @@ public class Messaging extends Observable {
 	private Messaging() {
 		staff = new StaffManager();
 		staff.refreshStaffList();
+
+		this.refreshMessages();
+
+		MessageTimer timer = new MessageTimer();
+		timer.run();
 
 	}
 
@@ -41,22 +47,45 @@ public class Messaging extends Observable {
 		return instance;
 	}
 
-	public void sendIRCMessage(String message) {
-		boolean result = ConnectionManager.sendChatMessageRequest(0, message);
+	public void sendIRCMessage(final String message) {
+		this.sendChatMessage(0, message);
 
-		this.notifyMessage(result);
 	}
 
-	public void sendPrivateMessage(String message, StaffMember receiver) {
-		boolean result = ConnectionManager.sendChatMessageRequest(receiver.getId(), message);
+	public void sendPrivateMessage(final String message, final StaffMember receiver) {
+		this.sendChatMessage(receiver.getId(), message);
 
-		this.notifyMessage(result);
 	}
 
 	public void refreshMessages() {
-		this.refreshIRCMessages();
+		class Refresher extends Thread {
 
-		this.refreshPrivateMessages();
+			@Override
+			public void run() {
+				refreshIRCMessages();
+
+				refreshPrivateMessages();
+			}
+		};
+
+		Refresher refresher = new Refresher();
+		refresher.run();
+
+	}
+
+	private void sendChatMessage(final int id, final String message) {
+		class MessageSender extends Thread {
+
+			@Override
+			public void run() {
+				boolean result = ConnectionManager.sendChatMessageRequest(id, message);
+
+				notifyMessage(result);
+			}
+		}
+
+		MessageSender sender = new MessageSender();
+		sender.run();
 	}
 
 	private void refreshIRCMessages() {
