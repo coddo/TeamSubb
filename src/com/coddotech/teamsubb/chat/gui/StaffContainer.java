d3152eaae70ef0dd797dfc4fa905867bbf94192c
Coddo <codobanclaudiu@gmail.com>
2014-02-10 19:43:50 +0200

    Extracted some important methods for the GUI classes into an interface

---
+++ src/com/coddotech/teamsubb/chat/gui/StaffContainer.java
@@ -0,0 +1,27 @@
+package com.coddotech.teamsubb.chat.gui;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Table;
+
+
+public class StaffContainer extends Composite {
+
+	private Table staff;
+	
+	public StaffContainer(Composite arg0, int arg1) {
+		super(arg0, arg1);
+		
+	}
+	
+	@Override
+	public void dispose() {
+		
+		
+		super.dispose();
+	}
+	
+	public void removeItem(StaffItem item) {
+		
+	}
+
+}
